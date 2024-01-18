a753aeb382db722c10deb987126b87bbe6269de1
Nick <kontakt@nick-krueger.de>
2024-01-19 00:02:12 +0100

    sell items in nonstop now

--- games/kings2/mainLoop.js
+++ games/kings2/mainLoop.js
@@ -12,21 +12,147 @@ let rounds = 0;
 // win 10 gaming Monitor, Bluestacks, fullscreen, menü rechts weggeklappt
 // tabbar noch sichtbar
 
+async function sellStuffInsideOfItemSlot(){
+
+    for (let i = 0; i < 10; i++) {
+
+     //click on first slot
+     robot.moveMouse(863, 763)
+     robot.mouseClick("left")
+     await new Promise(resolve => setTimeout(resolve, 1000));
+
+     //check if there is a item (Verkaufen button)
+     if(colorInSpot({x: 1188, y: 648},
+        "8c6dde")){
+
+             //for green arrow as a sign for an
+             // update
+             const isUpgrade = colorInArea({x: 852, y: 360},
+                 {x: 1140, y: 407},"63d219")
+
+             if(isUpgrade) {
+                 console.log("isUpgrade")
+                 robot.moveMouse(879, 653)
+                 await new Promise(resolve => setTimeout(resolve, 200));
+                 robot.mouseClick("left")
+             } else {
+                 console.log("is not an Upgrade")
+                 robot.moveMouse(1182, 636)
+                 await new Promise(resolve => setTimeout(resolve, 200));
+                 robot.mouseClick("left")
+                 await new Promise(resolve => setTimeout(resolve, 1000));
+                 //if legendary we have to confirm
+                 if(colorInSpot({x: 953, y: 550},
+                     "9475de")){
+                         robot.moveMouse(953, 550)
+                         await new Promise(resolve => setTimeout(resolve, 200));
+                         robot.mouseClick("left")
+                         await new Promise(resolve => setTimeout(resolve, 500));
+                         
+                 }
+             }
+
+     } else {
+         break;
+     }
+    }
+    // close window
+    robot.moveMouse(952, 149)
+    robot.mouseClick("left")
+
+}
+
 async function mainLoop(screenSize, stopBot) {
 
     //if in home menu
-    if(colorInSpot({x: 1069, y: 981},
-        "217dce")){
-           
-            //choose mission
-            robot.moveMouse(1161, 494)
-            robot.mouseClick("left")
+    if(colorInSpot({x: 839, y: 987},
+        "ff9309")){
 
+            if(rounds >= 7){
+                //open inventory
+                robot.moveMouse(841, 985)
+                await new Promise(resolve => setTimeout(resolve, 250));
+                robot.mouseClick("left")
+                await new Promise(resolve => setTimeout(resolve, 2000));
+
+                //is inventory open?
+                if(colorInSpot({x: 1027, y: 811},
+                    "5cc4ff")){
+
+                        //open hand slot
+                        robot.moveMouse(741, 520)
+                        await new Promise(resolve => setTimeout(resolve, 250));
+                        robot.mouseClick("left")
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        await sellStuffInsideOfItemSlot()
+
+                        //boots
+                        robot.moveMouse(745, 635)
+                        await new Promise(resolve => setTimeout(resolve, 250));
+                        robot.mouseClick("left")
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        await sellStuffInsideOfItemSlot()
+
+                        //head
+                        robot.moveMouse(1173, 412)
+                        await new Promise(resolve => setTimeout(resolve, 250));
+                        robot.mouseClick("left")
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        await sellStuffInsideOfItemSlot()
+
+                        //chest
+                        robot.moveMouse(1178, 518)
+                        await new Promise(resolve => setTimeout(resolve, 250));
+                        robot.mouseClick("left")
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        await sellStuffInsideOfItemSlot()
+
+                        //shield
+                        robot.moveMouse(1176, 625)
+                        await new Promise(resolve => setTimeout(resolve, 250));
+                        robot.mouseClick("left")
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        await sellStuffInsideOfItemSlot()
+
+                }
+
+                console.log("sold all items")
+                rounds = 0;
+            }
+            
+            await new Promise(resolve => setTimeout(resolve, 1000));
+           //click on Map
+           robot.moveMouse(734, 986)
+           await new Promise(resolve => setTimeout(resolve, 250));
+            robot.mouseClick("left")
+            await new Promise(resolve => setTimeout(resolve, 250));
+            //click dungeon
+            robot.moveMouse(972, 393)
+            await new Promise(resolve => setTimeout(resolve, 250));
+            robot.mouseClick("left")
+            await new Promise(resolve => setTimeout(resolve, 250));
+            //choose mission
+            robot.moveMouse(1158, 548)
+            await new Promise(resolve => setTimeout(resolve, 250));
+            robot.mouseClick("left")
+            await new Promise(resolve => setTimeout(resolve, 250));
             await new Promise(resolve => setTimeout(resolve, 2000));
  
             //bestätigen ohne Schlüssel
-            robot.moveMouse(948, 676)
+            robot.moveMouse(837, 697)
+            await new Promise(resolve => setTimeout(resolve, 250));
             robot.mouseClick("left")
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+            //Bestätigen - gilde auswählen
+            robot.mouseClick("left")
+            await new Promise(resolve => setTimeout(resolve, 250));
+
+            
+
+            console.log("next run")
+            rounds++;
+            await new Promise(resolve => setTimeout(resolve, 1000));
     }
 
     // if in fight
@@ -36,8 +162,6 @@ async function mainLoop(screenSize, stopBot) {
             robot.moveMouse(958, 163)
             robot.mouseClick("left")
         }
-
-          
 
                // atk 1    
              robot.moveMouse(756, 948)
@@ -51,13 +175,7 @@ async function mainLoop(screenSize, stopBot) {
                   // atk 3
             robot.moveMouse(1018, 943)
             robot.mouseClick("left")
-           
-
-            
-
-           
-
-
+      
 
         return;
     }
